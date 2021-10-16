bd13f73b0d8b27723e9b817af74a32beb4a991b2
levydvm <92640694+levydvm@users.noreply.github.com>
2021-10-16 12:34:04 -0500

    Update autoteeres.meta.js

--- autoteeres.meta.js
+++ autoteeres.meta.js
@@ -1,4 +1,4 @@
 // ==UserScript==
 // @name         Chateau Golf NettyCaddy Auto Tee Time Reservation
-// @version      0.2
-// @downloadURL  https://raw.githubusercontent.com/levydvm/auto-tee-time-reservation/main/autoteeres.js
+// @version      1.0
+// @downloadURL  https://cdn.jsdelivr.net/gh/levydvm/auto-tee-time-reservation/autoteeres.js
