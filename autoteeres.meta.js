f221c473162f15a4796bd14361ddfdc177d2262a
levydvm <92640694+levydvm@users.noreply.github.com>
2021-10-16 10:49:21 -0500

    Create autoteeres.meta.js

---
+++ autoteeres.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Chateau Golf NettyCaddy Auto Tee Time Reservation
+// @version      0.1
+// @description  Automatically book tee times at Chateau Golf and Country Club through NetCaddy's online reservation platform based on user preferences
+// @downloadURL  https://github.com/levydvm/auto-tee-time-reservation/autoteeres.js
+// @include      https://www.chateaugcc.com/Default.aspx?p=DynamicModule&pageid=41*
+// @include      https://www.chateaugcc.com/dialog.aspx*
+// @icon         https://www.google.com/s2/favicons?domain=chateaugcc.com
+// @run-at       document-idle
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant        GM_addStyle
+// ==/UserScript==
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
