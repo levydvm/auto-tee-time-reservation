5558531c6214007bea7a726e1f02062863324fed
levydvm <92640694+levydvm@users.noreply.github.com>
2021-10-16 10:55:50 -0500

    Update autoteeres.js

--- autoteeres.js
+++ autoteeres.js
@@ -2,7 +2,7 @@
 // @name         Chateau Golf NettyCaddy Auto Tee Time Reservation
 // @version      0.2
 // @description  Automatically book tee times at Chateau Golf and Country Club through NetCaddy's online reservation platform based on user preferences
-// @downloadURL  https://github.com/levydvm/auto-tee-time-reservation/autoteeres.js
+// @downloadURL  https://github.com/levydvm/auto-tee-time-reservation/blob/main/autoteeres.js
 // @include      https://www.chateaugcc.com/Default.aspx?p=DynamicModule&pageid=41*
 // @include      https://www.chateaugcc.com/dialog.aspx*
 // @icon         https://www.google.com/s2/favicons?domain=chateaugcc.com
